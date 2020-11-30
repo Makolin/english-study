fe2fed790be23a6cef5e971b70cad853eb3f03e9
Михаил <makolin@bk.ru>
2020-11-30 18:18:24 +0500

    Добавьте файлы проекта.

---
+++ EnglishStudy/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace EnglishStudy
+{
+    class Program
+    {
+        // Вывод коллекции по одной строке на каждый из времен Simple
+        static public bool Show(int currentLevel)
+        {
+            var AllEnglsihString = new TranslationAction();
+            AllEnglsihString.OneLevelStudy(currentLevel);
+
+            foreach (var oneString in AllEnglsihString.ExportTranslationList)
+            {
+                Console.WriteLine(oneString.russianLanguage);
+                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(oneString.englishLanguage);
+                Console.ResetColor();
+            }
+            Console.WriteLine("--------------------Конец списка---------------------");
+
+            // Блок для повторного запуска блока изучения
+            while (true)
+            {
+                Console.WriteLine("\nДля повтора нажмите Enter, а для выхода Escape.");
+                Console.Write("Введенная команда: > ");
+                var pressKey = Console.ReadKey();
+
+                if (pressKey.Key == ConsoleKey.Enter) return true;
+                if (pressKey.Key == ConsoleKey.Escape) return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите уровень тренировки по английскому языку.\nНа текущий момент доступно два уровня: 1 или 2.");
+                Console.Write("Введенный уровень для изучения: > ");
+                var enterText = Console.ReadLine();
+                var continueStudy = false;
+                int writeLevel;
+
+                switch (enterText)
+                {
+                    case "1":
+                        writeLevel = 1;
+                        break;
+                    case "2":
+                        writeLevel = 2;
+                        break;
+                    default:
+                        writeLevel = -1;
+                        break;
+                }
+
+                if (writeLevel > 0)
+                {
+                    continueStudy = Show(writeLevel);
+                    if (!continueStudy) return;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод. Повторите попытку.");
+                }
+            }
+        }
+    }
+}
---
+++ EnglishStudy/Translation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnglishStudy
+{
+    public enum EnglishTense
+    {
+        FutureSimpleAffirmative,
+        FutureSimpleNegative,
+        FutureSimpleQuestions,
+        PresentSimpleAffirmative,
+        PresentSimpleNegative,
+        PresentSimpleQuestions,
+        PastSimpleAffirmative,
+        PastSimpleNegative,
+        PastSimpleQuestions
+    }
+
+    public class Translation
+    {
+        public int levelStudy;
+        public EnglishTense tense;
+        public string englishLanguage;
+        public string russianLanguage;
+        public Translation()
+        {
+
+        }
+        public Translation(int levelStudy, EnglishTense tense, string englishLanguage, string russianLanguage)
+        {
+            this.levelStudy = levelStudy;
+            this.tense = tense;
+            this.englishLanguage = englishLanguage;
+            this.russianLanguage = russianLanguage;
+        }
+        public List<EnglishTense> SortRandom()
+        {
+            var sortRandomEnum = new List<EnglishTense>();
+            foreach (EnglishTense currentTense in Enum.GetValues(typeof(EnglishTense)))
+            {
+                sortRandomEnum.Add(currentTense);
+            }
+            sortRandomEnum = sortRandomEnum.OrderBy(t => Guid.NewGuid().ToString()).ToList();
+            return sortRandomEnum;
+        }
+    }
+}
